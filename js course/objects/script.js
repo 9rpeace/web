dcb199de5a718ead2eeb58187409bdfdac692f9c
9rpeace <9rpeace@gmail.com>
2020-06-02 14:09:27 +0300

    fix

---
+++ js course/objects/script.js
@@ -0,0 +1,15 @@
+
+let user = {
+    name: "John",
+    secondName: "Doe",
+    age: 30,
+    
+};
+user.isAdmin = true;
+alert (user.isAdmin);
+alert (user.name);
+
+alert (user.age);
+delete user.age;
+
+alert (user.age);
