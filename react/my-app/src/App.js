10ebfc1076f79cdee110b4ecf05614d27e137a10
9rpeace <9rpeace@gmail.com>
2020-06-03 16:05:18 +0300

    react

---
+++ react/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return(
+    <div>
+      <h1>Hello world</h1>
+      <button variants="primary">button</button>
+    </div>
+    
+  );
+}
+
+export default App;
