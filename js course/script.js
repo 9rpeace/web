bd049e5e422f5d15293ef64a5249d2e20563d8a3
9rpeace <9rpeace@gmail.com>
2020-06-10 13:13:21 +0300

    js

--- js course/script.js
+++ js course/script.js
@@ -1,3 +1,21 @@
-function showMessage() {
-    alert('Hello, im function');
+let count = +prompt('How many cats must be drawn', 'Enter number here');
+let smile = prompt('1 for :), 2 for :(, 3 for -_-, cat for guess what');
+
+switch (smile) {
+    case 1: smile = ':)'
+        break;
+    case 2: smile = ':('
+        break;
+    case 3: smile = '-_-'
+        break;
+    case 'cat': smile = '=^.^='
+        break;
 }
+
+function drawCats(howManyTimes, whatToDraw) {
+    for (let i=0; i < howManyTimes; i++) {
+        console.log(i + " " + whatToDraw);
+    }
+}
+
+drawCats(count, smile);
